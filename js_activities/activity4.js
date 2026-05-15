0ff86ebaf4af71fc38832aa5e5d1548ca18dfd78
Jinky Lopez <jinky.a.lopez@accenture.com>
2026-05-15 09:58:01 +0000

    completed activity4 - triangle perimeter

--- js_activities/activity4.js
+++ js_activities/activity4.js
@@ -1,2 +1,13 @@
 /*Create a function that calculates the perimeter of a triangle. Function should return the 
 value of perimeter. */
+
+var side1 = 9;
+var side2 = 6;
+var side3 = 3;
+
+function getTrianglePerimeter(side1, side2, side3) {
+    return side1 + side2 + side3;
+}
+
+var trianglePerimeter = getTrianglePerimeter(side1, side2, side3);
+console.log("Perimeter = " + trianglePerimeter);
